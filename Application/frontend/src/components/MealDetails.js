429d577b86cc2d638c4e6cd157e2bd7c749cc5fb
MlepNos <kaan.case2110@gmail.com>
2023-11-06 14:34:22 +0100

    change edit layout ,add functionality to edit button

--- Application/frontend/src/components/MealDetails.js
+++ Application/frontend/src/components/MealDetails.js
@@ -1,52 +1,156 @@
-import { useMealsContext } from '../hooks/useMealsContext'
-
+import { useMealsContext } from "../hooks/useMealsContext";
+import { useState } from "react";
 //date fns
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
-const MealDetails = ({meal}) => {
-    const { dispatch } = useMealsContext(); //grab dispatch function
-    const handleClickDelete = async () => {
+const MealDetails = ({ meal }) => {
+  //STATES
+  const [isEditing, setIsEditing] = useState(false);
+  const [emptyFields, setEmptyFields] = useState([]);
+  const [isVegetarian, setIsVegetarian] = useState(meal.isVegetarian);
+  const [isVegan, setIsVegan] = useState(meal.isVegan);
+  const [hasGluten, setHasGluten] = useState(meal.hasGluten);
+  const [type, setType] = useState(meal.type);
+  const [name, setName] = useState(meal.name);
 
-        
-        const response = await fetch('/api/meals/' + meal._id ,{
-            method:'DELETE',   
-        })
-        const json = await response.json();
+  //DISPATCH
+  const { dispatch } = useMealsContext(); //grab dispatch function
 
-        if(response.ok){
-            dispatch({type: 'DELETE_MEAL', payload: json});
-        }
+  //HANDLE FUNCTIONS
+  const handleClickDelete = async () => {
+    const response = await fetch("/api/meals/" + meal._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_MEAL", payload: json });
     }
+  };
 
-    const handleClickEdit = async () => {
+  const handleClickEdit = () => {
+    setIsEditing(!isEditing);
+  };
+  const handleEditMeal = async () => {
+    const response = await fetch("/api/meals/" + meal._id, {
+      method: "PATCH",
+      body: JSON.stringify({
+        ...meal,
+        isVegetarian: isVegetarian,
+        isVegan: isVegan,
+        hasGluten: hasGluten,
+        type: type,
+        name: name,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
 
-        
-        const response = await fetch('/api/meals/' + meal._id ,{
-            method:'PATCH',   
-        })
-        const json = await response.json();
+    if (response.ok) {
+      dispatch({ type: "EDIT_MEAL", payload: json });
+      setIsEditing(false);
+    }
+  };
+  //RETURN
+  return (
+    <div className="meal-details">
+      {!isEditing && (
+        <div>
+          <h4>{meal.name}</h4>
+          <p>
+            <strong> isVegetarian: </strong>
+            {meal.isVegetarian}
+          </p>
+          <p>
+            <strong> isVegan: </strong>
+            {meal.isVegan}
+          </p>
+          <p>
+            <strong> hasGluten: </strong>
+            {meal.hasGluten}
+          </p>
+          <p>
+            <strong> type: </strong>
+            {meal.type}
+          </p>
 
-        if(response.ok){
-            dispatch({type: 'EDIT_MEAL', payload: json});
-        }
-    }    
-    return(
-        <div className="meal-details">
-            <h4>{meal.name}</h4>
-            <p><strong> isVegetarian: </strong>{meal.isVegetarian}</p>
-            <p><strong> isVegan: </strong>{meal.isVegan}</p>
-            <p><strong> hasGluten: </strong>{meal.hasGluten}</p>
-            <p><strong> type: </strong>{meal.type}</p>
-    
-            <p>{formatDistanceToNow(new Date(meal.createdAt), {addSuffix: true})}</p>
-
-            <div className='box'>
-                <button className="material-symbols-outlined-delete" onClick={handleClickDelete}>delete</button>
-            
-                <button className="material-symbols-outlined-edit" onClick={handleClickEdit} >edit</button>
-            </div>      
+          <p>
+            {formatDistanceToNow(new Date(meal.createdAt), { addSuffix: true })}
+          </p>
         </div>
-    )
-}
+      )}
+      {isEditing && (
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <label>IsVegetarian:</label>
+          <input
+            type="text"
+            onChange={(e) => setIsVegetarian(e.target.value)}
+            value={isVegetarian}
+          />
+          <label>isVegan:</label>
+          <input
+            type="text"
+            onChange={(e) => setIsVegan(e.target.value)}
+            value={isVegan}
+          />
+          <label>hasGluten:</label>
+          <input
+            type="text"
+            onChange={(e) => setHasGluten(e.target.value)}
+            value={hasGluten}
+          />
+          <label>type:</label>
+          <input
+            type="text"
+            onChange={(e) => setType(e.target.value)}
+            value={type}
+          />
+        </div>
+      )}
+      <div className="box">
+        {!isEditing && (
+          <button
+            className="material-symbols-outlined-edit"
+            onClick={handleClickEdit}
+          >
+            edit
+          </button>
+        )}
+        {isEditing && (
+          <button
+            className="material-symbols-outlined-edit"
+            onClick={handleEditMeal}
+          >
+            save
+          </button>
+        )}
+        {isEditing && (
+          <button
+            className="material-symbols-outlined-edit"
+            onClick={handleClickEdit}
+          >
+            cancel
+          </button>
+        )}
+        {isEditing && (
+          <button
+            className="material-symbols-outlined-delete"
+            onClick={handleClickDelete}
+          >
+            delete
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default MealDetails
+export default MealDetails;
--- Application/frontend/src/components/MealForm.js
+++ Application/frontend/src/components/MealForm.js
@@ -1,106 +1,100 @@
-import { useState } from "react"
-import { useMealsContext } from '../hooks/useMealsContext'
-
+import { useState } from "react";
+import { useMealsContext } from "../hooks/useMealsContext";
 
 const MealForm = () => {
-    //**** 
-    const { dispatch } = useMealsContext();
-    //**** 
-    const [name, setName] = useState('');
-    const [isVegetarian, setIsVegetarian] = useState('');
-    const [isVegan, setIsVegan] = useState('');
-    const [hasGluten, setHasGluten] = useState('');
-    const [type, setType] = useState('');
-    const [error, setError] = useState(null);
-    const [emptyFields, setEmptyFields] = useState([]);
+  //****
+  const { dispatch } = useMealsContext();
+  //****
+  const [name, setName] = useState("");
+  const [isVegetarian, setIsVegetarian] = useState("");
+  const [isVegan, setIsVegan] = useState("");
+  const [hasGluten, setHasGluten] = useState("");
+  const [type, setType] = useState("");
+  const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const meal = {name,isVegetarian,isVegan,hasGluten,type};
+    const meal = { name, isVegetarian, isVegan, hasGluten, type };
 
-        const response = await fetch('/api/meals',{
-            method:'POST',
-            body: JSON.stringify(meal),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const json = await response.json();
+    const response = await fetch("/api/meals", {
+      method: "POST",
+      body: JSON.stringify(meal),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
 
-        if(!response.ok){
-          
-            setError(json.error);
-            
-            setEmptyFields(json.emptyFields);
-            
-        }
-        if(response.ok){
-   
-            setName('');
-            setIsVegetarian('');
-            setIsVegan('');
-            setHasGluten('');
-            setType('');
-            setError(null);
-            setEmptyFields([]);
+    if (!response.ok) {
+      setError(json.error);
 
-            console.log('New Meal Added',json);
-            //console.log(workout);
-
-            dispatch({type: 'CREATE_MEAL', payload: json})
-        }
+      setEmptyFields(json.emptyFields);
     }
+    if (response.ok) {
+      setName("");
+      setIsVegetarian("");
+      setIsVegan("");
+      setHasGluten("");
+      setType("");
+      setError(null);
+      setEmptyFields([]);
 
-    return(
-        <form className="create" onSubmit={handleSubmit}> 
-            <h3>Add a New Meal</h3>
-            <label>Name:</label>
-            <input
-                type= "text"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                className={emptyFields.includes('name') ? 'error' : ''}
-            />
+      console.log("New Meal Added", json);
+      //console.log(workout);
 
-            <label>IsVegetarian:</label>
-            <input
-                type= "text"
-                onChange={(e) => setIsVegetarian(e.target.value)}
-                value={isVegetarian}
-                className={emptyFields.includes('isVegetarian') ? 'error' : ''}
+      dispatch({ type: "CREATE_MEAL", payload: json });
+    }
+  };
 
-            />
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a New Meal</h3>
+      <label>Name:</label>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        className={emptyFields.includes("name") ? "error" : ""}
+      />
 
-            <label>isVegan:</label>
-            <input
-                type= "text"
-                onChange={(e) => setIsVegan(e.target.value)}
-                value={isVegan}
-                className={emptyFields.includes('isVegan') ? 'error' : ''}   
-            />
+      <label>IsVegetarian:</label>
+      <input
+        type="text"
+        onChange={(e) => setIsVegetarian(e.target.value)}
+        value={isVegetarian}
+        className={emptyFields.includes("isVegetarian") ? "error" : ""}
+      />
 
-            <label>hasGluten:</label>
-            <input
-                type= "text"
-                onChange={(e) => setHasGluten(e.target.value)}
-                value={hasGluten}
-                className={emptyFields.includes('hasGluten') ? 'error' : ''}  
-            />
+      <label>isVegan:</label>
+      <input
+        type="text"
+        onChange={(e) => setIsVegan(e.target.value)}
+        value={isVegan}
+        className={emptyFields.includes("isVegan") ? "error" : ""}
+      />
 
-            <label>type:</label>
-            <input
-                type= "text"
-                onChange={(e) => setType(e.target.value)}
-                value={type}
-                className={emptyFields.includes('type') ? 'error' : ''}  
-            />
+      <label>hasGluten:</label>
+      <input
+        type="text"
+        onChange={(e) => setHasGluten(e.target.value)}
+        value={hasGluten}
+        className={emptyFields.includes("hasGluten") ? "error" : ""}
+      />
 
-            <button variant="contained">Add Meal</button>
-            {error && <div className="error">{error}</div>}
-            
-        </form> 
-    )
-}
+      <label>type:</label>
+      <input
+        type="text"
+        onChange={(e) => setType(e.target.value)}
+        value={type}
+        className={emptyFields.includes("type") ? "error" : ""}
+      />
 
-export default MealForm
+      <button variant="contained">Add Meal</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default MealForm;
