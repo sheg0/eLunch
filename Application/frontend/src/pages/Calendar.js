7f588278e2dcca640ee06e368f46b3ae9b1d109f
shego <esbait01@hs-esslingen.de>
2023-11-10 15:07:06 +0100

    add sidebar component

--- Application/frontend/src/pages/Calendar.js
+++ Application/frontend/src/pages/Calendar.js
@@ -1,11 +1,13 @@
+import ClippedDrawer from "../components/Sidebar.jsx";
+
 const Calendar = ({ meals }) => {
   //***const [workouts, setWorkouts] = useState(null);
   //useEffect will fire a function when the component is rendered ( the [] at the en will tell that the function should be called once)
 
   return (
-    <div className="calender">
-      <p>IM THE CALENDAR</p>
-    </div>
+
+    <ClippedDrawer />
+
   );
 };
 
