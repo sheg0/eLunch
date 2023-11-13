690af1b8f93efa931eb2a4e89fecf0d6e8290137
selimcetin <selim-cetin@web.de>
2023-11-13 21:27:52 +0100

    remove unnecessary ReactStrictMode (only for dev mode)

--- Application/frontend/src/index.jsx
+++ Application/frontend/src/index.jsx
@@ -5,20 +5,21 @@ import App from "./App";
 import { MealContextProvider } from "./context/MealContext";
 import { BrowserRouter } from "react-router-dom";
 import { EventContextProvider } from "./context/EventContext";
-import { ReactKeycloakProvider } from '@react-keycloak/web'
+import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./keycloak";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ReactKeycloakProvider authClient={keycloak} initOptions={ {onLoad: 'login-required', pkceMethod: "S256"} }>
-  //<React.StrictMode>
-  <EventContextProvider>
-    <MealContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </MealContextProvider>
-  </EventContextProvider>
-  //</React.StrictMode>
+  <ReactKeycloakProvider
+    authClient={keycloak}
+    initOptions={{ onLoad: "login-required", pkceMethod: "S256" }}
+  >
+    <EventContextProvider>
+      <MealContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MealContextProvider>
+    </EventContextProvider>
   </ReactKeycloakProvider>
 );
