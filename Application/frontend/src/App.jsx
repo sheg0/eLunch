9bf0ac8130cf6875b50c3dd1dc2269eae0de3a66
MlepNos <kaan.case2110@gmail.com>
2023-12-05 21:42:35 +0100

    delete dead code

--- Application/frontend/src/App.jsx
+++ Application/frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Container } from "@mui/material";
 import FetchMeal from "./components/FetchMeal";
 import FetchEvent from "./components/FetchEvent";
 import ClippedDrawer from "./components/Sidebar";
-import Fetch from "./components/Fetch";
 
 function App() {
   return (
@@ -11,7 +10,8 @@ function App() {
       <ClippedDrawer>
         <div className="pages">
           <Container>
-            <Fetch></Fetch>
+            <FetchMeal></FetchMeal>
+            <FetchEvent></FetchEvent>
           </Container>
         </div>
       </ClippedDrawer>
