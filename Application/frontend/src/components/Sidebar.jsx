c2d002cf1bff922dfecedbb232c92ee31a5560eb
imraaan41 <imran.sevinc12@gmail.com>
2023-12-06 10:54:49 +0100

    add keycloak information to profile

--- Application/frontend/src/components/Sidebar.jsx
+++ Application/frontend/src/components/Sidebar.jsx
@@ -16,7 +16,6 @@ import EuroIcon from "@mui/icons-material/Euro";
 import PeopleIcon from "@mui/icons-material/People";
 import StackedLineChartIcon from "@mui/icons-material/StackedLineChart";
 import InfoIcon from "@mui/icons-material/Info";
-//import SteinbeisLogo from "../images/Logo_Steinbeis_EST_white.png";
 import Profile from "./Profile/Profile";
 
 import { useNavigate } from "react-router-dom";
@@ -89,9 +88,6 @@ export default function ClippedDrawer({ children }) {
           },
         }}
       >
-        <Profile />
-        {/*<Toolbar />*/}
-
         <Box
           sx={{
             overflow: "auto",
@@ -124,6 +120,7 @@ export default function ClippedDrawer({ children }) {
               );
             })}
           </List>
+          <Profile />
           <Typography
             sx={{
               fontFamily: "Segoe UI",
