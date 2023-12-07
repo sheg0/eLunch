8bede91f77ae2759834f64f3b29cf4168800ea01
MlepNos <kaan.case2110@gmail.com>
2023-12-07 14:40:37 +0100

    reworked fetch

--- Application/frontend/src/components/MealList/MealList.jsx
+++ Application/frontend/src/components/MealList/MealList.jsx
@@ -13,6 +13,7 @@ import Checkbox from "@mui/material/Checkbox";
 import { useKeycloak } from "@react-keycloak/web";
 import Meal from "../Meal.jsx";
 
+import { useKeycloak } from "@react-keycloak/web";
 function MealList({ meals }) {
   // States
   const [isEditing, setIsEditing] = useState(false);
@@ -80,6 +81,7 @@ function MealList({ meals }) {
 
   return (
     <Container>
+      <button onClick={keycloak.logout}></button>
       <div className="table-con">
         <table className="table">
           <thead>
@@ -127,78 +129,8 @@ function MealList({ meals }) {
           </tbody>
         </table>
       </div>
-      {isEditing && (
-        <div className="create">
-          <h3>Add a New Meal</h3>
 
-          <InputField
-            label="Name:"
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-          <div className="checkbox">
-            <label>isLactoseFree</label>
-            <Checkbox
-              onChange={(e) => setisLactoseFree(e.target.value)}
-              value={isLactoseFree}
-              inputProps={{ "aria-label": "controlled" }}
-            />
-          </div>
-          <div className="checkbox">
-            <label>isGlutenFree</label>
-            <Checkbox
-              onChange={(e) => setisGlutenFree(e.target.value)}
-              value={isGlutenFree}
-              inputProps={{ "aria-label": "controlled" }}
-            />
-          </div>
-          <div className="checkbox">
-            <label>isWithAlcohol</label>
-            <Checkbox
-              onChange={(e) => setisWithAlcohol(e.target.value)}
-              value={isWithAlcohol}
-              inputProps={{ "aria-label": "controlled" }}
-            />
-          </div>
-          <div className="checkbox">
-            <label>isWithMeat</label>
-            <Checkbox
-              onChange={(e) => setIsWithMeat(e.target.value)}
-              value={isWithMeat}
-              inputProps={{ "aria-label": "controlled" }}
-            />
-          </div>
-
-          <div className="checkbox">
-            <label>IsVegetarian</label>
-            <Checkbox
-              onChange={(e) => setIsVegetarian(e.target.value)}
-              value={isVegetarian}
-              inputProps={{ "aria-label": "controlled" }}
-            />
-          </div>
-
-          <div className="checkbox">
-            <label>isVegan</label>
-            <Checkbox
-              onChange={(e) => setIsVegan(e.target.value)}
-              value={isVegan}
-              inputProps={{ "aria-label": "controlled" }}
-            />
-          </div>
-
-          <InputField
-            label="type:"
-            type="text"
-            onChange={(e) => setCategory(e.target.value)}
-            value={category}
-          />
-
-          <AddButton variant="contained">Add Meal</AddButton>
-        </div>
-      )}
-      <MealModal></MealModal>
+      <Modal></Modal>
     </Container>
   );
 }
