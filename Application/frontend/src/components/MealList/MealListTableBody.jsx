a0ec24724da0887dee3238367bab5ca7807963f2
shego <esbait01@hs-esslingen.de>
2023-12-22 23:30:10 +0100

    add "are you sure...?" modal to delete button in meal list

--- Application/frontend/src/components/MealList/MealListTableBody.jsx
+++ Application/frontend/src/components/MealList/MealListTableBody.jsx
@@ -1,10 +1,20 @@
 import MealTableData from "./MealTableData.jsx";
-import { FaPen } from "react-icons/fa";
-import { FaRegTrashAlt } from "react-icons/fa";
+import { FaPen, FaRegTrashAlt } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
 import { useMealListContext } from "../../hooks/useMealListContext.js";
+import React, { useState } from "react";
 
 const MealListTableBody = ({ meals, handleClickDelete }) => {
   const { deleteMeal, handleClickMealEdit } = useMealListContext();
+  const [selectedMeal, setSelectedMeal] = useState(null);
+
+  const openDeleteModal = (meal) => {
+    setSelectedMeal(meal);
+  };
+
+  const closeDeleteModal = () => {
+    setSelectedMeal(null);
+  };
 
   return (
     <tbody>
@@ -24,11 +34,37 @@ const MealListTableBody = ({ meals, handleClickDelete }) => {
               <div className="icon-gap"></div>
 
               <button
-                onClick={() => deleteMeal(meal)}
+                onClick={() => openDeleteModal(meal)}
                 className="icon-button delete"
               >
                 <FaRegTrashAlt />
               </button>
+
+              {selectedMeal && selectedMeal.id === meal.id && (
+                <div className="mealDelete-Modal">
+                  <div className="mealDelete-content">
+                    <button
+                      className="mealDelete-close"
+                      onClick={closeDeleteModal}
+                    >
+                      <RxCross1 />
+                    </button>
+                    <p>
+                      Möchten Sie wirklich <strong>{selectedMeal.name}</strong>{" "}
+                      löschen?
+                    </p>
+                    <button
+                      className="MealDelete-Button"
+                      onClick={() => {
+                        deleteMeal(selectedMeal);
+                        closeDeleteModal();
+                      }}
+                    >
+                      Löschen
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
           </td>
         </tr>
