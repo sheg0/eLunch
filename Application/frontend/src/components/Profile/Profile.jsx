a00553281e4f678e2b172d94c5dfc75ff3faef65
imraaan41 <imran.sevinc12@gmail.com>
2024-01-14 00:19:56 +0100

    add statistic in profile for mitessen and mitgegessen

--- Application/frontend/src/components/Profile/Profile.jsx
+++ Application/frontend/src/components/Profile/Profile.jsx
@@ -3,13 +3,20 @@ import "./Profile.css";
 import { useKeycloak } from "@react-keycloak/web";
 
 import { IoIosLogOut } from "react-icons/io";
-import { PiCookingPotBold } from "react-icons/pi";
+import { PiCookingPot } from "react-icons/pi";
 import { PiForkKnifeDuotone } from "react-icons/pi";
-import { useContext } from "react";
+
 import { useFinanceContext } from "../../hooks/useFinanceContext";
-const Profile = () => {
+import { EventDetailModal } from "../Modal/EventDetailModal/EventDetailModal";
+import { useCalendarContext } from "../../hooks/useCalendarContext";
+import { CalendarContext } from "../../context/CalendarContext";
+
+const Profile = ({ participants }) => {
   const { keycloak, initialized } = useKeycloak();
   const { finance, balance, setBalance } = useFinanceContext();
+  const { mitessenCount, setMitessenCount } = useState("");
+  const { isCookCount, setIsCookCount } = useState("");
+
   var username = "MM";
   var firstName = "Max";
   var lastName = "Mustermann";
@@ -56,12 +63,37 @@ const Profile = () => {
     keycloak.logout();
   };
 
+  const calculateStatistics = () => {
+    if (!participants) return;
+
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+    const mitessen = participants.filter(
+      (participant) =>
+        participant.hasMitessen &&
+        new Date(participant.timestamp) > thirtyDaysAgo
+    ).length;
+
+    const isCook = participants.filter(
+      (participant) =>
+        participant.isCook && new Date(participant.timestamp) > thirtyDaysAgo
+    ).length;
+
+    setMitessenCount(mitessen);
+    setIsCookCount(isCook);
+  };
+
+  // Rufe die Berechnungen auf, wenn sich die Teilnehmer ändern (z.B., wenn die Daten aktualisiert werden)
+  useEffect(() => {
+    calculateStatistics();
+  }, [participants]);
   return (
     <div className="profile-container">
       <div className="profile-content">
-        <div className="box1">
+        <div className="profile-box1">
           <div className="profile-circle">
-            <div className="circle-name">
+            <div className="profile-circle-name">
               {firstName[0]} {lastName[0]}
             </div>
           </div>
@@ -69,16 +101,22 @@ const Profile = () => {
             <IoIosLogOut></IoIosLogOut>
           </div>
         </div>
-        <div className="box2">
-          <div className="name">
+        <div className="profile-box2">
+          <div className="profile-name">
             {firstName} {lastName}
           </div>
-          <div className="balance">{balance || "1453"}</div>
+          <div className="profile-balance">{balance || "1453"}</div>
         </div>
-        <div className="box3">
-          <div className="statistic">Statistik:</div>
-          <PiCookingPotBold className="icon-gekocht"></PiCookingPotBold>
-          <PiForkKnifeDuotone className="icon-gegessen"></PiForkKnifeDuotone>
+        <div className="profile-box3">
+          <div className="profile-statistic">Statistik:</div>
+
+          <PiCookingPot className="profile-statistic-iconGekocht">
+            {isCookCount}
+          </PiCookingPot>
+
+          <PiForkKnifeDuotone className="profile-statistic-iconGegessen">
+            {mitessenCount}
+          </PiForkKnifeDuotone>
         </div>
       </div>
     </div>
