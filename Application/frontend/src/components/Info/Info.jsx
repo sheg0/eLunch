042a135fa3db40ec8db97faec02b3bc9889b538e
imraaan41 <imran.sevinc12@gmail.com>
2024-01-14 18:57:34 +0100

    add final styling to info page

--- Application/frontend/src/components/Info/Info.jsx
+++ Application/frontend/src/components/Info/Info.jsx
@@ -8,93 +8,102 @@ import Veggie from "../../images/Veggie.png";
 
 function Info() {
   return (
-    <div className="Info-Page-Container">
-      <div className="Info-Page-Content">
-        <p className="Info-Page-Headline">Informationen</p>
-
-        <p className="Info-Page-Liste">
-          <li>täglich Wasserspender desinfizieren und Auffangschale leeren</li>
-          <li>bei Bedarf Kaffeeautomat reinigen und Auffangschale leeren</li>
-          <li>Spülmaschine in der Regel nach dem Mittagessen laufen lassen</li>
-          <li>14-tägig schauen was nachbestellt werden muss (Grundzutaten)</li>
-          <li>
-            Wenn ein Bedarf festgestellt wird, diesen im digitalen
-            Einkaufszettel notieren
-          </li>
-          <li>monatlich Altglas entsorgen</li>
-          <li>monatlich Oberflächen in der Küche gründlich renigen</li>
-          <li>
-            monatlich Inhalte der Kühlschränke prüfen und alte Sachen entsorgen
-          </li>
-          <li>quartalsweise Vorratsschrank putzen und neu einräumen</li>
-          <li>
-            2x im Jahr die Küche gründlich reinigen, auch auf den Schränken
-          </li>
-        </p>
-      </div>
-
-      <div className="Info-Page-Legend">
-        <img
-          className="Info-Page-Image"
-          src={Alcohol}
-          alt="Alcohol"
-          title="Mit Alkohol"
-        />
-        <p className="Info-Page-ImageText">Mit Alkohol</p>
-        <img
-          className="Info-Page-Image"
-          src={Dairyfree}
-          alt="Dairyfree"
-          title="Laktosefrei"
-        />
-        <p className="Info-Page-ImageText">Laktosefrei</p>
-        <img
-          className="Info-Page-Image"
-          src={Glutenfree}
-          alt="Glutenfree"
-          title="Glutenfrei"
-        />
-        <p className="Info-Page-ImageText">Glutenfrei</p>
-        <img
-          className="Info-Page-Image"
-          src={Meat}
-          alt="Meat"
-          title="Mit Fleisch"
-        />
-        <p className="Info-Page-ImageText">Mit Fleisch</p>
-        <img
-          className="Info-Page-Image"
-          src={Vegan}
-          alt="Vegan"
-          title="Vegan"
-        />
-        <p className="Info-Page-ImageText">Vegan</p>
-        <img
-          className="Info-Page-Image"
-          src={Veggie}
-          alt="Veggie"
-          title="Vegetarisch"
-        />
-        <p className="Info-Page-ImageText">Vegetarisch</p>
-      </div>
-      <div className="InfoPage-Footer">
-        <div>
-          <h2 className="InfoPage-Footer-Headline">Hier finden Sie uns</h2>
-          <p className="InfoPage-Text">
-            Steinbeis Systems Technology Group <br />
-            Martinstraße 42-44 <br /> D-73728 Esslingen
-          </p>
+    <>
+      <div className="Infp-Container">
+        <h1 className="Info-Headline"> Informationen </h1>
+        <div className="Info-Content">
+          <div className="Info-Row">
+            <ul className="Info-Liste">
+              <li>
+                Täglich Wasserspender desinfizieren und Auffangschale leeren
+              </li>
+              <li>
+                Bei Bedarf Kaffeeautomat reinigen und Auffangschale leeren
+              </li>
+              <li>
+                Spülmaschine in der Regel nach dem Mittagessen laufen lassen
+              </li>
+              <li>
+                14-tägig schauen was nachbestellt werden muss (Grundzutaten)
+              </li>
+              <li>
+                Wenn ein Bedarf festgestellt wird, diesen im digitalen
+                Einkaufszettel notieren
+              </li>
+              <li> Monatlich Altglas entsorgen</li>
+              <li> Monatlich Oberflächen in der Küche gründlich renigen</li>
+              <li>
+                Monatlich Inhalte der Kühlschränke prüfen und alte Sachen
+                entsorgen
+              </li>
+              <li>Quartalsweise Vorratsschrank putzen und neu einräumen</li>
+              <li>
+                2x im Jahr die Küche gründlich reinigen, auch auf den Schränken
+              </li>
+            </ul>
+            <div className="Info-Legend">
+              <img
+                className="Info-Image"
+                src={Alcohol}
+                alt="Alcohol"
+                title="Mit Alkohol"
+              />
+              <img
+                className="Info-Image"
+                src={Dairyfree}
+                alt="Dairyfree"
+                title="Laktosefrei"
+              />
+              <img
+                className="Info-Image"
+                src={Glutenfree}
+                alt="Glutenfree"
+                title="Glutenfrei"
+              />
+              <img
+                className="Info-Image"
+                src={Meat}
+                alt="Meat"
+                title="Mit Fleisch"
+              />
+              <img
+                className="Info-Image"
+                src={Vegan}
+                alt="Vegan"
+                title="Vegan"
+              />
+              <img
+                className="Info-Image"
+                src={Veggie}
+                alt="Veggie"
+                title="Vegetarisch"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <h2 className="InfoPage-Footer-Headline">Nehmen Sie Kontakt auf</h2>
-          <p className="InfoPage-Text">
-            Telefon: +49 711 99596-300 <br />
-            Fax: +49 711 99596-301 <br />
-            Email: info@steinbeis-stg.de
-          </p>
+        <div className="Info-Footer">
+          <div className="Info-Column">
+            <h2 className="Info-Footer-Headline">Hier finden Sie uns</h2>
+            <p className="Info-Text">
+              Steinbeis Systems Technology Group <br />
+              Martinstraße 42-44 <br /> D-73728 Esslingen
+            </p>
+          </div>
+          <div className="Info-Column">
+            <h2 className="Info-Footer-Headline">Nehmen Sie Kontakt auf</h2>
+            <p className="Info-Text">
+              Telefon: +49 711 99596-300 <br />
+              Fax: +49 711 99596-301 <br />
+              Email: info@steinbeis-stg.de
+            </p>
+          </div>
+        </div>
+        <div className="Verewigung">
+          Diese Website wurde von Esra Balci, Vivian Schmiss, Imran-Nur-Reyhan
+          Sevinc, M. Kaan Asik und Selim Cetin erstellt
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
