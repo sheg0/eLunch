95beb490e58e5725cb5340326d66d306d324bb5e
imraaan41 <imran.sevinc12@gmail.com>
2024-01-15 10:34:39 +0100

    deletet a variable

--- Application/frontend/src/components/Sidebar/Sidebar.jsx
+++ Application/frontend/src/components/Sidebar/Sidebar.jsx
@@ -12,7 +12,7 @@ import SteinbeisLogo from "../../images/Logo_Steinbeis_EST_white.png";
 
 function Sidebar({ children }) {
   const navigate = useNavigate();
-  const [selectedTab, setSelectedTab] = useState("Speiseplan");
+  const [selectedTab, setSelectedTab] = useState();
   const [sidebarVisible, setSidebarVisible] = useState(false);
 
   const toggleSidebar = () => {
